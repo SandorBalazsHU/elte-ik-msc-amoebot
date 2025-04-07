418eb6f6b78f0bf8bcc082fda0acd7b1696de0c8
Sándor Balázs <sabuaai@INF.ELTE.HU>
2025-04-08 01:23:15 +0200

    Adding wall and comments.

--- src/scene.py
+++ src/scene.py
@@ -13,6 +13,7 @@ class SceneType(Enum):
     RANDOM = auto()
     CONNECTED = auto()
     CENTER = auto()
+    WALL = auto()
     META_MODUL = auto()
     SETTINGS = auto()
     EXIT = auto()
@@ -34,6 +35,7 @@ class Scene:
             SceneType.RANDOM: self.setup_random_scene,
             SceneType.CONNECTED: self.setup_connected_motion_scene,
             SceneType.CENTER: self.setup_center_motion_scene,
+            SceneType.WALL: self.setup_wall_motion_scene,
             SceneType.META_MODUL: self.setup_meta_modul_motion_scene,
             SceneType.SETTINGS: self.settings,
             SceneType.EXIT: self.exit
@@ -58,6 +60,7 @@ class Scene:
         self.menu_object.add.button("Random", lambda: self.set_scene(SceneType.RANDOM))
         self.menu_object.add.button("Connected motion", lambda: self.set_scene(SceneType.CONNECTED))
         self.menu_object.add.button("To center", lambda: self.set_scene(SceneType.CENTER))
+        self.menu_object.add.button("Wall", lambda: self.set_scene(SceneType.WALL))
         self.menu_object.add.button("Meta modul", lambda: self.set_scene(SceneType.META_MODUL))
         self.menu_object.add.button("Settings", lambda: self.set_scene(SceneType.SETTINGS))
         self.menu_object.add.button("Exit", lambda: self.set_scene(SceneType.EXIT))
@@ -88,19 +91,19 @@ class Scene:
 
     def setup_connected_motion_scene(self):
         BOT_NUMBER = 12
-        for i in range(1, BOT_NUMBER + 1):
+        for i in range(2, BOT_NUMBER + 1):
             bot = Amoebot(self.simulation.triangle_map, i, 2)
             bot.set_behavior(BehaviorType.TO_HEADING)
             bot.set_heading(3)
             self.simulation.amoebots.append(bot)
 
-        for i in range(1, BOT_NUMBER + 1):
+        for i in range(2, BOT_NUMBER + 1):
             bot = Amoebot(self.simulation.triangle_map, i, 1)
             bot.set_behavior(BehaviorType.TO_HEADING)
             bot.set_heading(3)
             self.simulation.amoebots.append(bot)
 
-        for i in range(1, BOT_NUMBER + 1):
+        for i in range(2, BOT_NUMBER + 1):
             bot = Amoebot(self.simulation.triangle_map, i, 0)
             bot.set_behavior(BehaviorType.TO_HEADING)
             bot.set_heading(3)
@@ -112,10 +115,11 @@ class Scene:
         for bot in self.simulation.amoebots:
             bot.set_behavior(BehaviorType.INTELLIGENT)
             bot.set_intelligent_behavior(Behavior.center_seek_behavior)
-
+    
     def setup_meta_modul_motion_scene(self):
         bots = self.create_meta_modul(start_row=0, start_col=0, rows=4, cols=4, color=(255,0,0))
         leader = bots[3][3]
+        leader.color=(100,0,0)
         leader.set_state(AmoebotState.ACTIVE)
         leader.set_behavior(BehaviorType.INTELLIGENT)
         leader.set_intelligent_behavior(Behavior.zigzag_behavior)
@@ -123,16 +127,33 @@ class Scene:
 
         bots2 = self.create_meta_modul(start_row=8, start_col=7, rows=4, cols=6, color=(255,255,0))
         leader2 = bots2[0][0]
+        leader2.color=(100,100,0)
         leader2.set_state(AmoebotState.ACTIVE)
         leader2.set_behavior(BehaviorType.TO_HEADING)
         leader2.set_heading(2)
         self.simulation.commanded_bots.append(leader2)
 
         wall = self.create_meta_modul(start_row=6, start_col=0, rows=2, cols=6, color=(255,255,255))
-
         wall2 = self.create_meta_modul(start_row=0, start_col=Config.Grid.COLS-2, rows=Config.Grid.COLS, cols=2, color=(255,255,255))
+    
+    def setup_wall_motion_scene(self):
+        wall = self.create_meta_modul(start_row=6, start_col=0, rows=2, cols=10, color=(255,255,255))
+        wall2 = self.create_meta_modul(start_row=0, start_col=Config.Grid.COLS-2, rows=Config.Grid.COLS, cols=2, color=(255,255,255))
+        create_random_moving_amoebots(self, count=40, x_min=0, x_max=12, y_min=0, y_max=5)
+        create_random_moving_amoebots(self, count=40, x_min=0, x_max=12, y_min=8, y_max=14)    
 
     def create_meta_modul(self, start_row: int, start_col: int, rows: int, cols: int, color = None):
+        """
+        Létrehoz egy összekapcsolt NxM-es amőbotblokkot a megadott kezdőpozíciótól.
+        Minden botot hozzáad a szimulációhoz, PASSIVE állapotra állít, és a megadott színt kapja.
+        
+        :param start_row: Kezdősor
+        :param start_col: Kezdőoszlop
+        :param rows: Blokk sorainak száma
+        :param cols: Blokk oszlopainak száma
+        :param color: Az amőbotok színe (RGB tuple)
+        :return: 2D lista az amőbotokkal
+        """
         bots = [[None for _ in range(cols)] for _ in range(rows)]
         for r in range(rows):
             for c in range(cols):
@@ -150,3 +171,21 @@ class Scene:
                 if r + 1 < rows:
                     current.connect(bots[r + 1][c])
         return bots
+
+def create_random_moving_amoebots(self, count: int, x_min: int, x_max: int, y_min: int, y_max: int):
+    """
+    Véletlenszerűen létrehoz X darab mozgó amőbotot a megadott koordináta-tartományban.
+    Minden bot ACTIVE állapotot és RANDOM viselkedést kap, véletlen kezdőirányból indulva.
+
+    :param count: Amőbotok száma
+    :param x_min: Minimum oszlop
+    :param x_max: Maximum oszlop
+    :param y_min: Minimum sor
+    :param y_max: Maximum sor
+    """
+    for _ in range(count):
+        col = random.randint(x_min, x_max)
+        row = random.randint(y_min, y_max)
+        bot = Amoebot(self.simulation.triangle_map, row, col)
+        bot.set_behavior(BehaviorType.RANDOM)
+        self.simulation.amoebots.append(bot)
